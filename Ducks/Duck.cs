27b881086e919e2c46ed17d5c1be82cbc8691e9b
jvalentine <jvalentine@webjogger.com>
2017-06-28 20:45:41 -0400

    Separate classes, edit documentation
    Separate classes, edit documentation

---
+++ Ducks/Duck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    public abstract class Duck { 
+        
+            public bool floats { get; set; }
+            public bool flies { get; set; }
+            public bool quacks { get; set; }
+            public int legs { get; set; }
+            public int eyes { get; set; }
+        
+    }
+
+    // Mallard inherits properties from Duck
+    class Mallard : Duck
+    {
+        // Custom attributes for a Mallard duck
+        public string madeOf { get; set; }
+    }
+   
+    // Wooden inherits properties from Duck
+    class Wooden : Duck
+    {
+        // Custom attributes for a Wooden duck
+        public string madeOf { get; set; }
+    }
+
+    // Rubber inherits properties from Duck
+
+    class Rubber : Duck
+    {
+        // Custom attributes for a Rubber duck
+        public string madeOf { get; set; }
+        public string name;
+        public string color { get; set; }
+    }
+
+
+}
--- Ducks/Program.cs
+++ Ducks/Program.cs
@@ -6,38 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ducks
 {
-    abstract class Duck
-    {
-        public bool floats { get; set; }
-        public bool flies { get; set; }
-        public bool quacks { get; set; }
-        public int legs { get; set; }
-        public int eyes {get;set;} 
-    }
-
-    // Mallard inherits properties from Duck
-    class Mallard : Duck
-    {
-        // Custom attributes for a Mallard duck
-        public string madeOf { get; set; }
-    }
-
-    // Wooden inherits properties from Duck
-    class Wooden : Duck
-    {
-        // Custom attributes for a Wooden duck
-        public string madeOf { get; set; }
-    }
-
-    // Rubber inherits properties from Duck
-    class Rubber : Duck
-    {
-        // Custom attributes for a Rubber duck
-        public string madeOf { get; set; }
-        public string name;
-        public string color { get; set; }
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -47,24 +15,24 @@ namespace Ducks
             {
                 liveDuck.floats = true;
                 liveDuck.flies = true;
-                liveDuck.quacks = false;
+                liveDuck.quacks = true;
                 liveDuck.legs = 2;
-                liveDuck.madeOf = "meat";
                 liveDuck.eyes = 2;
+                liveDuck.madeOf = "meat";
+                
             }
-            Mallard deadDuck = new Mallard();
-            {
-                deadDuck.eyes = 1;
-            }
-
-            
+       
             // Instantiate a new Wooden Duck   
             Wooden woodDuck = new Wooden();
-            woodDuck.floats = true;
-            woodDuck.flies = false;
-            woodDuck.quacks = false;
-            woodDuck.legs = 0;
-            woodDuck.madeOf = "wood";
+            {
+                woodDuck.floats = true;
+                woodDuck.flies = false;
+                woodDuck.quacks = false;
+                woodDuck.legs = 0;
+                woodDuck.eyes = 0;
+                woodDuck.madeOf = "wood";
+            }
+            
 
             // Instantiate a new Rubber Duck
             Rubber rubberDucky = new Rubber();
@@ -73,27 +41,33 @@ namespace Ducks
                 rubberDucky.flies = false;
                 rubberDucky.quacks = false;
                 rubberDucky.legs = 0;
+                rubberDucky.eyes = 0;
                 rubberDucky.name = "George";
-                rubberDucky.madeOf = "rubber"; // madeOf is broken because the access modifier is wrong. The access modifier on madeOf is missing. It should be there, and should be public (not private).
-
+                rubberDucky.madeOf = "rubber";
+                rubberDucky.color = "yellow";
             }
 
+            // Instantiate a new Rubber Duck with a custom color
             Rubber redRubberDucky = new Rubber();
             {
                 redRubberDucky.color = "red";
             }
 
             // You cant instantiate the class Duck because it is an abstract class.
-            Duck newDuck = new Duck();
+             Duck newDuck = new Duck();
 
             // Add one of each of the ducks to a List
             var ducks = new List<Duck>();
-            ducks.Add(rubberDucky);
-            ducks.Add(woodDuck);
-            ducks.Add(liveDuck);
-            ducks.Add(deadDuck);
 
-            // Iterate through the list
+            ducks.Add(liveDuck);
+            ducks.Add(woodDuck);
+            ducks.Add(rubberDucky);
+            ducks.Add(redRubberDucky);
+
+            // Remove one of the ducks from the List
+            ducks.Remove(woodDuck);
+
+           // Iterate through the list
             foreach (var d in ducks)
             {
                 bool DoesFloat;
@@ -106,8 +80,6 @@ namespace Ducks
                 Console.WriteLine("This Duck sinks!");
                 Console.ReadLine();
             }
-
-
 
             // What about the eyes? How would you implement giving the ducks eyes?
         }
